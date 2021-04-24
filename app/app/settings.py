671dfe27ef1a22ddb4feb9d8631eda5e663a6b67
ozeas souza <ozeas.souza@grupoboticario.com.br>
2021-04-24 16:12:27 -0300

    feat: added custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'core.User'
