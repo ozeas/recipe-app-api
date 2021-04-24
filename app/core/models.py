390d6b7bee62fb23817a4cb5b51ddcb982add1d5
ozeas souza <ozeas.souza@grupoboticario.com.br>
2021-04-24 18:59:47 -0300

    feat: updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with is successful"""
+        """Test creating a new user with is successfully"""
         email = 'test@local.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -17,16 +17,19 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
+        """Test new user with email normalized"""
         email = 'test@LOCAL.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
+        """Test new user with invalid email error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
     def test_create_new_superuser(self):
+        """Test new super user successfully"""
         user = get_user_model().objects.create_superuser(
             'test@local.com',
             'test123'
